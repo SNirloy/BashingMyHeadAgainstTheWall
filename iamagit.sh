c861db412dc1fd80415e149f03a3de467f0885b4
Sadi Nirloy <snirloy30@stuy.edu>
2023-03-08 13:53:47 -0500

    "ayo?"

--- iamagit.sh
+++ iamagit.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/bash
 
-git pull
-git add .
+$(git pull)
+$(git add *)
 
 echo "Type your Comment"
 read comment
 
-git commit -m \"$comment\"
-git push
+$(git commit -m \"$comment\")
+$(git push)
