dab37bece095470674c241ab3f5389f3c916eda8
SNirloy <yolrinidas@gmail.com>
2023-03-09 20:10:22 -0500

    Understanding command line arguments

--- iamagit.sh
+++ iamagit.sh
@@ -1,10 +1,15 @@
 #! /usr/bin/bash
 
-git pull
-git add *
+while getopts a: flag
+do 
+    case "${flag}" in
+        a) echo ${OPTARG} ;;
+    esac
+done
 
-echo "Type your Comment"
-read comment
-git commit -m "\"$comment\""
-git push
-echo "Donsies"
+i=1;
+for user in "$@" 
+do
+    echo "$i: $user";
+    i=$((i + 1));
+done
