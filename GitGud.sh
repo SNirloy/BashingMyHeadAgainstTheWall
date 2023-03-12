01df3d8be2a8a4d71bf5f9bbbfc241ee276703e0
SNirloy <yolrinidas@gmail.com>
2023-03-12 14:39:10 -0400

    may this work

--- GitGud.sh
+++ GitGud.sh
@@ -5,9 +5,20 @@
 #To be named "IAmAGit.sh"
 
 # version 1
-read -p "Enter Comment: " comment;
+# read -p "Enter Comment: " comment;
 
-git pull;
-git add .;
+# git pull;
+# git add .;
+# git commit -m "$comment";
+# git push;
+
+# version 2
+for input in $@
+do
+    # echo $input
+    $(git add $input);
+    shift 1;
+done
+read -p "Enter Comment: " comment;
 git commit -m "$comment";
 git push;
