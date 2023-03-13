fdd3b2fdbe5f1aa3afd32432450c5fdf7b62a45a
SNirloy <yolrinidas@gmail.com>
2023-03-12 20:19:19 -0400

    GitGud Version 2 done

--- GitGud.sh
+++ GitGud.sh
@@ -13,10 +13,38 @@
 # git push;
 
 # version 2
+# for input in $@
+# do
+    # echo $input
+    # if [[ $input == "-p" ]]
+    # then 
+        # $(git pull);
+    # elif [[ $input == "-a" ]]
+    # then 
+        # $(git add .);
+    # else 
+        # $(git add $input);
+    # fi
+    # shift 1;
+# done
+# read -p "Enter Comment: " comment;
+# git commit -m "$comment";
+# git push;
+
+# version 3
+
 for input in $@
 do
     # echo $input
-    if [[ $input == "-a" ]]
+    if [[ $input == "-p" ]]
+    then 
+        $(git pull);
+    elif [[ $input == "-b" ]]
+    then 
+        shift 1;
+        echo $@;
+        continue;
+    elif [[ $input == "-a" ]]
     then 
         $(git add .);
     else 
