df7e7b9b99678d13d2f08b3e2919d786fbe1f542
SNirloy <yolrinidas@gmail.com>
2023-03-12 20:36:10 -0400

    All Good

--- GitGud.sh
+++ GitGud.sh
@@ -33,25 +33,26 @@
 
 # version 3
 
+pushing(){
+    read -p "Enter Comment: " comment;
+    git commit -m "$comment";
+    git push;
+}
+
+push=0;
+
 for input in $@
 do
     # echo $input
-    if [[ $input == "-p" ]]
-    then 
-        $(git pull);
-    elif [[ $input == "-b" ]]
-    then 
-        shift 1;
-        echo $@;
-        continue;
-    elif [[ $input == "-a" ]]
-    then 
-        $(git add .);
-    else 
-        $(git add $input);
-    fi
+    case $input in
+        "-p") $(git pull);;
+        "-b") shift 1;
+            git branch $@;
+            git checkout $@;;
+        "-a") $(git add .);
+            push=1;;
+        *) $(git add $input);
+            push=1; 
+    esac
     shift 1;
 done
-read -p "Enter Comment: " comment;
-git commit -m "$comment";
-git push;
