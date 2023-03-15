98afd63ed8054ccc9b88add70b61ab16e26ef136
SNirloy <yolrinidas@gmail.com>
2023-03-15 01:49:51 -0400

    branching is good

--- IAmAGit.sh
+++ IAmAGit.sh
@@ -1,17 +1,41 @@
 #! /usr/bin/bash
 
-for input in $@
+pushing(){
+    read -p "Enter Comment: " comment;
+    git commit -m "$comment";
+    git push;
+}
+
+push=0;
+
+i=1;
+while [[ $i -le $# ]]
 do
-    if [[ $input == "-a" ]]
-    then 
-        $(git add .);
-    elif [[ $input == "-p" ]]
-    then 
-        $(git pull);
-    else 
-        $(git add $input);
-    fi
+    plus=1;
+    case $1 in
+        "-p") 
+            git pull;;
+        "-b")
+            plus=2;
+            git branch $2;
+            git checkout $2;;
+        "-m")
+            plus=2;
+            git checkout $2;
+            git pull origin main;
+            git merge --no-ff main && git checkout main;
+            git merge --no-ff $2;;
+        "-a") 
+            $(git add .);
+            push=1;;
+        *) 
+            $(git add $1);
+            push=1;;
+    esac
+    i=$(($i + $plus));
+    shift $plus;
 done
-read -p "Enter Comment: " comment;
-git commit -m "$comment";
-git push;
+if [[ $push -gt 0 ]] 
+then 
+    pushing;
+fi 
