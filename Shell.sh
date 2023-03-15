d54e759896427cc18e0244a6c566092d78e54ed9
Sadi Nirloy <snirloy30@stuy.edu>
2023-03-15 13:18:22 -0400

    Hello

---
+++ Shell.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/bash
+for input in $@
+do
+    if [[ $input == "-a" ]]
+    then 
+        $(git add .);
+    elif [[ $input == "-p" ]]
+    then 
+        $(git pull);
+    else 
+        $(git add $input);
+    fi
+done
+read -p "Enter Comment: " comment;
+git commit -m "$comment";
+git push;
